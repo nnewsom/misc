c8786ca7349efde0b3de21530f1ec5042ee68f39
nnewsom <nowhereandnoone@mailinator.com>
2022-03-13 10:41:03 -0700

    updated arch installer since I ran into some issues recently. added comments for clarity

---
+++ arch_setup/stage1.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# this setup script is specifically for EFI systems
+# could be modified for BIOS but why bother in this day in age
+# to enable EFI on vbox: VM->Settings->System->Motherboard check and enable "Enable EFI (special OSes only)"
+
+# the script creates an encrypted LVM with 100% space allocated to root (/)
+# with no swap by default. it will chroot into the build env on completion
+# to execute stage2
+
+ROOT_SIZE="100%FREE"
+
+DEVICE="$1"
+if [ -z "$DEVICE" ]
+    then
+        echo "need path of disk to install. ie. /dev/sda, /dev/nvme0n1";
+        exit 1;
+fi
+echo "Installing on device $DEVICE"
+
+if [[ "$DEVICE" =~ ^/dev/nvme0.* ]]
+    then
+        PART_EFI="$DEVICE"p1;
+        PART_LVM="$DEVICE"p2;
+    else
+        PART_EFI="$DEVICE"1;
+        PART_LVM="$DEVICE"2;
+fi
+
+echo -e "target: $DEVICE\nEFI: $PART_EFI\nLVM: $PART_LVM"
+read -p "look correct? (y/N): " confirm && [[ $confirm == [yY] ]] || exit 1
+
+INSTALL_PACKAGES="arch-install-scripts"
+PACSTRAP_PACKAGES=\
+"base base-devel efibootmgr vim dialog xterm btrfs-progs "\
+"grub mkinitcpio linux linux-firmware lvm2 pacman-contrib intel-ucode"
+
+pacman -Sy
+pacman -S $INSTALL_PACKAGES --noconfirm
+
+cat << EOF | parted $DEVICE
+rm 1
+rm 2
+rm 3
+rm 4
+mklabel gpt
+mkpart ESP fat32 1Mib 512Mib
+set 1 boot on
+name 1 efi
+mkpart primary 512Mib 100%
+set 2 lvm on
+name 2 lvm
+print
+quit
+EOF
+
+modprobe dm-crypt
+modprobe dm-mod
+
+cryptsetup luksFormat -v -s 512 -h sha512 "$PART_LVM"
+cryptsetup open "$PART_LVM" luks_lvm
+pvcreate /dev/mapper/luks_lvm
+vgcreate arch /dev/mapper/luks_lvm
+lvcreate -n root -l "$ROOT_SIZE" arch
+
+mkfs.fat -F32 "$PART_EFI"
+mkfs.btrfs -L root /dev/mapper/arch-root
+
+mount /dev/mapper/arch-root /mnt
+mkdir -p /mnt/boot
+mount "$PART_EFI" /mnt/boot
+
+lsblk -f
+read -p "lv groups correct? (y/N): " confirm && [[ $confirm == [yY] ]] || exit 1
+pacstrap /mnt $PACSTRAP_PACKAGES --noconfirm
+genfstab -U -p /mnt > /mnt/etc/fstab
+
+cp ./stage2.sh /mnt/stage2.sh
+chmod +x /mnt/stage2.sh
+
+PART_LVM_UUID=`blkid -s UUID -o value $PART_LVM`
+BOOT_OPTIONS="cryptdevice=UUID=""$PART_LVM_UUID"":cryptlvm root=/dev/mapper/arch-root rw"
+sed -i "s,REPLACEMEBOOT_OPTIONSREPLACEME,$BOOT_OPTIONS,g" /mnt/stage2.sh
+
+echo "stage1 complete. dropping into build root chroot. exec stage2.sh to continue"
+arch-chroot /mnt /bin/bash
+echo "build complete"
---
+++ arch_setup/stage2.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+# this script is to be run inside arch-chroot of new environment
+# the script will do the following:
+# * install bootloader
+# * create and install initramfs
+# * set hostname
+# * set root passwd
+# * create limited user and set passwd
+# * setup local .rc files to user
+# * install packages listed in `PACKAGES`
+# * set timezone to US/los_angeles
+# * pick 6 fastest local US mirrors for pacman
+# * set local to en_US.UTF-8
+# * set system default umask
+# * enable networkmanager service
+# * install virtualbox guest if in vm (optional)
+
+PACKAGES=\
+"git wget i3lock i3blocks firefox xautolock "\
+"wpa_supplicant networkmanager alsa-utils "\
+"ttf-dejavu ttf-liberation i3-wm i3lock "\
+"lxappearance thunar network-manager-applet "\
+"terminator dmenu feh xorg-server "\
+"xorg-xinit arc-gtk-theme arc-icon-theme "\
+"i3status man python-pip python-virtualenv "\
+"strace polkit keepassxc rustup"
+
+# set up initram fs
+cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak
+echo 'adding `encrypt` and `lvm2` to hooks between `block` and `filesystems`'
+egrep ^HOOKS /etc/mkinitcpio.conf
+# need to add `encrypt` and `lvm2` after `block` but before `filesystems`
+sed -i 's/block filesystems/block encrypt lvm2 filesystems/g' /etc/mkinitcpio.conf
+egrep ^HOOKS /etc/mkinitcpio.conf
+read -p "modification correct? (y/N): " confirm && [[ $confirm == [yY] ]] || exit 1
+mkinitcpio -v -p linux
+
+# install bootloader
+pacman -S grub efibootmgr intel-ucode --noconfirm
+grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
+cp /etc/default/grub /etc/default/grub.bak
+echo "adding cryptdevice to grub cmdline"
+egrep ^GRUB_CMDLINE_LINUX_DEFAULT /etc/default/grub
+
+# the `REPLACEMEBOOT` tag will be replaced by stage1 to target the UUID of the crypt device
+sed -i \
+    's,GRUB_CMDLINE_LINUX_DEFAULT=",GRUB_CMDLINE_LINUX_DEFAULT="REPLACEMEBOOT_OPTIONSREPLACEME ,g' \
+    /etc/default/grub
+
+egrep ^GRUB_CMDLINE_LINUX_DEFAULT /etc/default/grub
+read -p "modification correct? (y/N): " confirm && [[ $confirm == [yY] ]] || exit 1
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# set sytsem information
+# hostname
+read -p "hostname: " hostname
+hostnamectl set-hostname "$hostname"
+# root password
+echo "setup root password"
+passwd root
+# limited user
+echo "setup user"
+read -p "username: " username
+useradd -G wheel,storage,power,network,audio -m "$username"
+passwd "$username"
+
+# set up local .rc files
+USER_HOMEDIR="/home/$username"
+XINITRC="$USER_HOMEDIR/.xinitrc"
+BASHRC="$USER_HOMEDIR/.bashrc"
+VIMRC="$USER_HOMEDIR/.vimrc"
+
+cat << EOF > "$XINITRC"
+#!/bin/bash
+eval \$(ssh-agent)
+export SSH_AUTH_SOCK
+export SSH_AGENT_PID
+exec i3
+EOF
+chown "$username":"$username" "$XINITRC"
+
+# set bashrc aliases
+cat << EOF >> "$BASHRC"
+alias vi='vim'
+alias cat='cat -v'
+alias view='vim -R'
+alias mv='mv -i'
+alias rm='rm -i'
+alias cp='cp -i'
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+PS1='[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\\$ '
+
+EOF
+chown "$username":"$username" "$BASHRC"
+
+cat << EOF >> "\$VIMRC"
+syntax on
+set tabstop=4
+set expandtab
+set softtabstop=4
+set showmatch
+set hlsearch
+set foldenable
+set foldlevelstart=10
+set rnu
+set number
+EOF
+chown "$username":"$username" "$VIMRC"
+
+# install desired base packages
+pacman -S $PACKAGES --noconfirm
+
+# set up timezone
+timedatectl set-timezone America/Los_Angeles
+
+# pick fatest local US mirrors
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+wget 'https://archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4' -O /etc/pacman.d/mirrorlist.new
+cat -v /etc/pacman.d/mirrorlist.new | egrep "(.edu|rackspace.com|kernel.org)/" | tr -d '#' > /etc/pacman.d/mirrorlist.new.2
+rankmirrors -n 6 /etc/pacman.d/mirrorlist.new.2 > /etc/pacman.d/mirrorlist
+pacman -Sy
+
+# set system locale to en_US.UTF-8
+echo "en_US.UTF-8 UTF-8 >> /etc/locale.gen"
+locale-gen
+echo "LANG=en_US.UTF-8 > /etc/locale.conf"
+
+# set system default umask to 027 ( rwxrw----- )
+sed -i 's/umask 022/umask 027/g' /etc/profile
+
+# enable network manager
+systemctl enable NetworkManager
+
+# Setups up configuration and common scripts from misc repo
+read -p "set up configurations and copy scripts for misc? (Y/n): " confirm && [[ $confirm != [nN] ]] || exit 1
+cd /tmp
+git clone --depth 1 https://github.com/nnewsom/misc.git
+mkdir -p "$USER_HOMEDIR/.config"
+cp -r misc/i3 "$USER_HOMEDIR/.config/"
+cp -r misc/terminator "$USER_HOMEDIR/.config/"
+cp -r misc/scripts "$USER_HOMEDIR/"
+chown -R "$username":"$username" "$USER_HOMEDIR"
+
+# set up virtualbox guest and enable core service
+# this doesn't enable clipboard, drag drop etc
+# those will need to be enabled manually with `VBoxClient --clipboard`
+read -p "virtualbox vm? (y/N): " confirm && [[ $confirm == [yY] ]] || exit 1
+pacman -S virtualbox-guest-utils --noconfirm
+systemctl enable vboxservice
