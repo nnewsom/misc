e62627c354c6aa25122b0c1b770d3fe93cbc9e9e
nick <user.localhost>
2022-03-15 12:05:17 -0700

    added some cscope scripts + cscope vim plugin

---
+++ cscope/regen_c.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# regenerate cscope for c projects from current directory
+IFS=$'\n'
+SRC_PATH="`pwd`"
+
+rm -v cscope.*
+find -L "$SRC_PATH" -type f \
+    -iname "*.cc" \
+    -o iname "*.c" \
+    -o iname "*.cxx" \
+    -o iname "*.cpp" \
+    -o iname "*.h" \
+    2>/dev/null | sed 's/^/"/;s/$"/' > cscope.files
+
+cscope -b -q -k
