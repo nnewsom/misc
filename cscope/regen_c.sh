fdfd351e1402e1b8779ce391c7fa79d42c08c7b0
nick <user.localhost>
2022-03-23 10:44:26 -0700

    fixed find scripts

--- cscope/regen_c.sh
+++ cscope/regen_c.sh
@@ -7,10 +7,10 @@ SRC_PATH="`pwd`"
 rm -v cscope.*
 find -L "$SRC_PATH" -type f \
     -iname "*.cc" \
-    -o iname "*.c" \
-    -o iname "*.cxx" \
-    -o iname "*.cpp" \
-    -o iname "*.h" \
+    -o -iname "*.c" \
+    -o -iname "*.cxx" \
+    -o -iname "*.cpp" \
+    -o -iname "*.h" \
     2>/dev/null | sed 's/^/"/;s/$"/' > cscope.files
 
 cscope -b -q -k
